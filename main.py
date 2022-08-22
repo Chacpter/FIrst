184093668a850219985f089d000fefe924841bba
Chacpter <111740612+Chacpter@users.noreply.github.com>
2022-08-22 19:29:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ def get_birthday():
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
+  words = /n+words
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
